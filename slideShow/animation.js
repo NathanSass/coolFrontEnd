d5fa7215386c4ab2913e9d33b26dd903a0e1500a
E750606 <nathan.sass@chase.com>
2014-08-17 09:55:12 -0700

    messin with styles

--- slideShow/animation.js
+++ slideShow/animation.js
@@ -1,31 +1,30 @@
-console.log("in js");
 var show = {
 	animateHelpWalkThrough: function(){
 		var phrases = ["Hi", "Let me walk you through this", "This is how you build a message queue", "I think it looks a lot better than a nested list of calls" ];
 
 			show._createNewMessage(phrases[0])
 				.fadeIn(800)
-				.delay(2000)
 				
+				.delay(2000)
 				.queue(function(n){
 					show._createNewMessage(phrases[1]);
 					n();
 				})
 				.fadeIn(500)
 				
-				.delay(5000)
+				.delay(2000)
 				.queue(function(n){
 					show._createNewMessage(phrases[2]);
 					n();
 				})
 				.fadeIn(500)
 				
-				.delay(500)
+				.delay(2000)
 				.queue(function(n){
 					show._createNewMessage(phrases[3]);
 					n();
 				})
-				.fadeIn(3000);
+				.fadeIn(1000);
 		},
 
 		_createNewMessage: function(message){
