f8a152e09ac0d1e1081dc2cd3b8399f2c21c8468
E750606 <nathan.sass@chase.com>
2014-08-17 20:45:08 -0700

    changed file structure

--- slideShow/queueFiles/animation.js
+++ slideShow/queueFiles/animation.js
@@ -1,6 +1,6 @@
 var show = {
 	animateHelpWalkThrough: function(){
-		var phrases = ["Hi", "Let me walk you through this", "This is how you build a message queue", "I think it looks a lot better than a nested list of calls" ];
+		var phrases = ["Hi, let's build a slideshow using jquery's queue!", "Let me walk you through this.", "You can execute any animations that you want. Don't be limited by jquery chaining!", "The code looks a lot cleaner than nesting inside jquery animation functions. Check it out in the readme!" ];
 
 			show._createNewMessage(phrases[0])
 				.fadeIn(800)
@@ -12,14 +12,14 @@ var show = {
 				})
 				.fadeIn(500)
 				
-				.delay(2000)
+				.delay(3000)
 				.queue(function(n){
 					show._createNewMessage(phrases[2]);
 					n();
 				})
 				.fadeIn(500)
 				
-				.delay(2000)
+				.delay(5000)
 				.queue(function(n){
 					show._createNewMessage(phrases[3]);
 					n();
