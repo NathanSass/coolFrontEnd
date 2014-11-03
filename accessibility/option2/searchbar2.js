98ff58488c738babdd7c06654cf3d54807cc91f9
E750606 <nathan.sass@chase.com>
2014-11-02 16:36:35 -0800

    about to add a second option where the background is a fixed size

---
+++ accessibility/option2/searchbar2.js
@@ -0,0 +1,38 @@
+var WIDGET = {
+	flexInputArea: function(){
+		var container = $(".search-bar").width();
+		var widgets = $(".left-icon").width() + $(".right-icon").width();
+		$(".search-input")[0].style.width = ((container - widgets - 41) + "px");
+	},
+
+	focusInputArea: function(e){
+		WIDGET._changeBackgroundColors("rgba(255, 255, 255, 0)");
+		WIDGET._showResultsArea();
+	},
+	resetSearchBar: function(e){
+		$('.results-container').hide();
+		$('.search-input').css("border-bottom", "none");
+		WIDGET._changeBackgroundColors("rgba(255, 255, 255, .5)");
+	},
+
+	_changeBackgroundColors: function(color){
+		var widgets = [$('.search-input'), $('.left-icon'), $('.right-icon')];
+		for(var i = 0; i < widgets.length; i++){
+			widgets[i][0].style.background = color;
+		}
+	},
+	_showResultsArea: function(){
+		$('.results-container').show();
+		$('.search-input').css("border-bottom", "1px solid grey");
+	}
+};
+
+$(function() {
+	WIDGET.flexInputArea();
+	$(document).on("click", ".search-input", WIDGET.focusInputArea);
+	$(document).on("mouseleave", ".results-container", WIDGET.resetSearchBar); //really need a scenario for mouseout on input as well
+
+  window.onresize=function(){
+  	WIDGET.flexInputArea();
+	};
+});
