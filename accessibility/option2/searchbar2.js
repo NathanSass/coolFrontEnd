5e18443c98c7b9269475e9c752887a541467fa48
E750606 <nathan.sass@chase.com>
2014-11-02 16:50:22 -0800

    functioning product

--- accessibility/option2/searchbar2.js
+++ accessibility/option2/searchbar2.js
@@ -6,20 +6,18 @@ var WIDGET = {
 	},
 
 	focusInputArea: function(e){
-		// WIDGET._changeBackgroundColors("rgba(255, 255, 255, 0)");
+		WIDGET._changeBackgroundColors("rgba(255, 255, 255, 1)");
 		WIDGET._showResultsArea();
 	},
 	resetSearchBar: function(e){
-		$('.results-container').hide();
+		$('.results').hide();
+		$('.results-container').css("height", "44px");
 		$('.search-input').css("border-bottom", "none");
 		WIDGET._changeBackgroundColors("rgba(255, 255, 255, .5)");
 	},
 
 	_changeBackgroundColors: function(color){
-		var widgets = [$('.search-input'), $('.left-icon'), $('.right-icon')];
-		for(var i = 0; i < widgets.length; i++){
-			widgets[i][0].style.background = color;
-		}
+			$(".results-container")[0].style.background = color;
 	},
 	_showResultsArea: function(){
 		$('.results').show();
@@ -31,7 +29,7 @@ var WIDGET = {
 $(function() {
 	WIDGET.flexInputArea();
 	$(document).on("click", ".search-input", WIDGET.focusInputArea);
-	// $(document).on("mouseleave", ".results-container", WIDGET.resetSearchBar); //really need a scenario for mouseout on input as well
+	$(document).on("mouseleave", ".results-container", WIDGET.resetSearchBar); //really need a scenario for mouseout on input as well
 
   window.onresize=function(){
   	WIDGET.flexInputArea();
